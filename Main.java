6533ce013699b67abdf613b79aa62b0b3ec52797
fahrimaulana <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2022-01-31 14:02:31 +0700

    Ubah system pemanggilan dengan menggunakan Scanner dan Switch Case

--- Main.java
+++ Main.java
@@ -1,39 +1,41 @@
+import java.util.Scanner;
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
+        System.out.println("Hewan apa yang ingin ditampilkan datanya?");
+        System.out.println("1:Singa \t2:Monyet \t3:Jerapah \t4:Ikan   \t5:Cheetah");
+        System.out.println("Masukkan kode angka: ");
+        int pilihan = sc.nextInt();
 
-        //Object singa
-        Singa iyah = new Singa();
-
-        Singa.makan();
-        Singa.suaranya();
-        Singa.umurnya();
-
-        //Object cheetah
-        Cheetah citah = new Cheetah();
-
-        Cheetah.makanan();
-        Cheetah.suara();
-        Cheetah.umur();
-
-        //Objek ikan
-        Ikan iwak = new Ikan();
-
-        Ikan.makanan();
-        Ikan.umur();
-        Ikan.suara();
-
-        //Objek monyet
-        Monyet ketek = new Monyet();
-
-        Monyet.suara();
-        Monyet.makanan();
-        Monyet.umur();
-
-        //Objek jerapah
-        Jerapah jrph = new Jerapah();
-
-        Jerapah.umur();
-        Jerapah.suara();
-        Jerapah.makanan();
+        switch (pilihan){
+            case 1:
+                Singa.makan();
+                Singa.suaranya();
+                Singa.umurnya();
+                break;
+            case 2:
+                Monyet.makanan();
+                Monyet.suara();
+                Monyet.umur();
+                break;
+            case 3:
+                Jerapah.makanan();
+                Jerapah.suara();
+                Jerapah.umur();
+                break;
+            case 4:
+                Ikan.makanan();
+                Ikan.suara();
+                Ikan.umur();
+                break;
+            case 5:
+                Cheetah.makanan();
+                Cheetah.suara();
+                Cheetah.umur();
+                break;
+            default:
+                System.out.println("Parameter tidak valid");
+        }
     }
 }
