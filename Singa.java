35a24194e6f017b5b824f3ecbe8097fb1b575fda
fahrimaulana <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2022-01-31 10:55:37 +0700

    iyah

---
+++ Singa.java
@@ -0,0 +1,15 @@
+public class Singa {
+    String makanan = "Daging";
+    String suara = "Meong";
+    int umur = 16;
+
+    void makan(){
+        System.out.println("Makanan singa adalah "+makanan);
+    }
+    void suaranya(){
+        System.out.println("Suara singa berbunyi "+suara);
+    }
+    void umurnya(){
+        System.out.println("Umur singa adalah "+umur);
+    }
+}
